9f591d2cdfe0b5b27bc3318e3baf8385efcf07c5
Guang <u6260778@anu.edu.au>
2018-10-15 17:36:58 +0800

    update4

--- Project/app/src/main/java/anu/comp2100/teamgg/god_of_hand/Stones.java
+++ Project/app/src/main/java/anu/comp2100/teamgg/god_of_hand/Stones.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.support.annotation.Nullable;
 
 import java.util.ArrayList;
 import java.util.Iterator;
@@ -12,6 +13,7 @@ import java.util.Random;
 public class Stones extends ArrayList<Stone> {
 
     Context context;
+
 
     Random r = new Random();
     int a = r.nextInt(4) + 1;
@@ -24,8 +26,15 @@ public class Stones extends ArrayList<Stone> {
         Iterator<Stone> it = this.iterator();
         while (it.hasNext()){
             Stone s = it.next();
-            if (s.item.Y > GameActivity.screenHight)
+            if (s.item.Y > GameActivity.screenHight){
+                float w = GameActivity.screenWidth;
+                float [] fl = {1/16 * w, 5/16 * w, 9/16 * w, 13/16 * w};
+                int i = (int) Math.random() * fl.length;
+                float y1 = (float) (Math.random() * GameActivity.screenHight/8);
                 it.remove();
+                Stone s1 = new Stone(fl[i], y1, s.item.width, s.item.length);
+                this.add(s1);
+            }
         }
     }
 
